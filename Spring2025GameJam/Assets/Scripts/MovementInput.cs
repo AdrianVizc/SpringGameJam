95dc1d734469ffdd9ed4f77ce17c6f6ce66c3dae
AdrianVizc <vizcara1@uci.edu>
2025-04-11 21:28:56 -0700

    added movement :)

---
+++ Spring2025GameJam/Assets/Scripts/MovementInput.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviour
+{
+    [SerializeField] float acceleration;
+    [SerializeField] float steering;
+    [SerializeField] float maxSpeed;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveInput = Input.GetAxis("Vertical");
+        float turnInput = Input.GetAxis("Horizontal");
+
+        rb.AddForce(transform.up * moveInput * acceleration);
+
+        if (rb.velocity.magnitude > 0.1f)
+        {
+            float direction = Mathf.Sign(Vector2.Dot(rb.velocity, transform.up));
+            rb.MoveRotation(rb.rotation - turnInput * steering * direction);
+        }
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+}
